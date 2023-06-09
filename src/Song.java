3a1ffe37cfdc96844b17775d20ebbdce13328791
unknown <salyi.mark@gmail.com>
2023-06-09 18:15:26 +0200

    playlist

---
+++ src/Song.java
@@ -0,0 +1,18 @@
+public class Song {
+    private String title;
+    private double duration;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
